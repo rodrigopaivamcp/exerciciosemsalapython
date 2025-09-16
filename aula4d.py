7693dd1bfdddb6de8f1f57965ad23d878c5d3baf
rodrigopaivamcp <rodrigo.mcpaiva@sempreceub.com>
2025-09-16 18:00:50 -0300

    Update aula4d.py

--- aula4d.py
+++ aula4d.py
@@ -5,7 +5,10 @@ def maximo (num1, num2):
         maior = num2
     return maior
 
-numero1 = int(input('Primeiro número: '))
-numero2 = int(input('Segundo número: '))
-vl_retorno = maximo(numero1, numero2)
+
+if __ name__ == '__main__':
+    numero1 = int(input('Primeiro número: '))
+    numero2 = int(input('Segundo número: '))
+    vl_retorno = maximo(numero1, numero2)
+
 print("\nMaior valor:", vl_retorno)
