0fb318883cd76a036306eb41a3f022d1b1e7123b
rodrigopaivamcp <rodrigo.mcpaiva@sempreceub.com>
2025-09-24 08:54:01 -0300

    Add files via upload

--- aula7.py
+++ aula7.py
@@ -1,6 +1,8 @@
 ct_aluno = 0
 soma_idade = 0
-mais_novo= 200    #mais_novo = float('inf')
+mais_novo= 200  # mais_novo = float('inf')
+mais_velho = float('-inf')
+ct_30 = 0
 print('digite 0 para sair da repetição')
 
 while True:
@@ -12,7 +14,16 @@ while True:
     if idade < mais_novo:
         media = soma_idade/ct_aluno
         mais_novo = idade
+    if idade > mais_velho:
+        mais_velho = idade
+    if idade > 30:
+        ct_30 = ct_30 + 1   # ct_30 += 1
+
+
 
 print(f'a media de idade da turma é de {media} anos')
-print(f'o aluno mais novo é {mais_novo}')
-
+print(f'o aluno mais novo tem {mais_novo}')
+print(f'a quantidade de alunos é {ct_aluno}')
+print(f'a soma das idades dos alunos é {soma_idade}')
+print(f'a idade do aluno mais velho é {mais_velho} anos')
+print(f'a quantidade de alunos com mais de 30 anos é de {ct_30}')
