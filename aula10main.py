d0c303d5dd6b9b652b58773e2a4b462b260c83ca
rodrigopaivamcp <rodrigo.mcpaiva@sempreceub.com>
2025-11-05 10:06:21 -0300

    Add files via upload

--- aula10main.py
+++ aula10main.py
@@ -4,17 +4,17 @@ if __name__ == '__main__':
     qd1 = Quadrado(cor ='azul', lado= 2)
     print(qd1)
 
-    print("\nQuadrado: ", qd1.get_cor(), "\nlado: ", qd1.get_cor(), "\narea:", qd1.get_area(), "\nperimetro", qd1.get_perimetro())
+    print("\nQuadrado: ", qd1.get_cor(), "\nlado: ", qd1.get_cor(), "\narea:", qd1.area(), "\nperimetro", qd1.perimetro())
 
     qd1.set_cor('Verde')
     qd1.set_lado(9)
 
     crc = Circulo(cor='vermelho', raio=4)
-    print('\n Circulo: ', crc.get_cor(), '\nRaio:', crc.get_raio(), crc.get_area(), crc.get_perimetro())
+    print('\n Circulo: ', crc.get_cor(), '\nRaio:', crc.get_raio(), crc.area(), crc.perimetro())
 
     crc.set_cor('Amarelo')
     crc.set_raio(5)
 
     print("\nApós alterações usando setters")
-    print("\nQuadrado:", qd1.get_cor(), "\nLado:", qd1.get_lado())
-    print("\nCirculo:", crc.get_cor(), "\nRaio:", crc.get_raio())
+    print("\nQuadrado:", qd1.get_cor(), "\nLado:", qd1.get_lado(), "\nÁrea:", qd1.area(), "Perímetro:", qd1.perimetro())
+    print("\nCirculo:", crc.get_cor(), "\nRaio:", crc.get_raio(), "\nÁrea:", qd1.area(), "Perímetro:", qd1.perimetro())
